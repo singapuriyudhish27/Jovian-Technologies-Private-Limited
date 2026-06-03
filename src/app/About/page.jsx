29c14333291ce634436bd3e1b0a7c274b7bd1c9d
yudhish-singapuri <yudhish.singapuri@reformiqo.com>
2026-06-03 14:20:45 +0530

    Build Error Fixed-1

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function AboutPage() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
